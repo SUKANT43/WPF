cbba4e7b408d2649239bfd7f5752fc93b170cb9c
SUKANT43 <sukant98657@gmail.com>
2026-02-06 13:36:35 +0530

    resources and resources dictionary

---
+++ Topics/ResourcesAndResourceDictionary/ResourcesAndResourceDictionary/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+
+namespace ResourcesAndResourceDictionary
+{
+    public partial class MainWindow : Window
+    {
+        private bool _isDark;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeTheme_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries[0] =
+                new ResourceDictionary
+                {
+                    Source = new Uri(
+                        _isDark
+                            ? "Resources/LightTheme.xaml"
+                            : "Resources/DarkTheme.xaml",
+                        UriKind.Relative)
+                };
+
+            _isDark = !_isDark;
+        }
+    }
+}
