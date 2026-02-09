742212636a263573cb840dfd6fd96cd19e35ec8c
SUKANT43 <sukant98657@gmail.com>
2026-02-09 11:53:39 +0530

    ICommand

---
+++ Topics/ICommandExamples/ICommandExamples/ButtonVm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ICommandExamples
+{
+    public class ButtonVm:INotifyPropertyChanged
+    {
+        public ICommand SayHelloCommand { get; }
+
+        public ButtonVm()
+        {
+            SayHelloCommand = new RelayCommand(SayHello);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SayHello()
+        {
+            MessageBox.Show("hi from ICommand");
+        }
+
+
+    }
+}
