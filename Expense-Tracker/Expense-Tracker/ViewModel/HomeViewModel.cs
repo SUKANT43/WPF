d63173818992e808470b644e49d3a2b68c67ae87
SUKANT43 <sukant98657@gmail.com>
2026-03-10 16:21:00 +0530

    code optimized

--- Expense-Tracker/Expense-Tracker/ViewModel/HomeViewModel.cs
+++ Expense-Tracker/Expense-Tracker/ViewModel/HomeViewModel.cs
@@ -7,8 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -17,29 +15,132 @@ namespace Expense_Tracker.ViewModel
     public class HomeViewModel : Observable
     {
         private MainViewModel _mainViewModel;
+
         public ICommand AddTransactionCommand { get; }
         public ICommand DeleteTransactionCommand { get; }
         public ICommand ClearFilterCommand { get; }
+
         private List<DataModel> _allTransactions = new List<DataModel>();
+
         public HomeViewModel(MainViewModel mainViewModel)
         {
             _mainViewModel = mainViewModel;
+
             AddTransactionCommand = new RelayCommand(o => AddTransaction());
             DeleteTransactionCommand = new RelayCommand(DeleteTransaction);
-            ClearFilterCommand = new RelayCommand(o=>ClearFilter());
+            ClearFilterCommand = new RelayCommand(o => ClearFilter());
+
             var user = UserSession.CurrentUser;
 
             if (user?.History != null)
-            {
                 _allTransactions = user.History.ToList();
-                Transactions = new ObservableCollection<DataModel>(_allTransactions);
-            }
-            else
+
+            Transactions = new ObservableCollection<DataModel>(_allTransactions);
+
+            UpdateData();
+        }
+
+        public Array TransactionTypes => Enum.GetValues(typeof(TransactionType));
+        public Array Categories => Enum.GetValues(typeof(Category));
+
+        private TransactionType _selectedTransactionType;
+        public TransactionType SelectedTransactionType
+        {
+            get => _selectedTransactionType;
+            set { _selectedTransactionType = value; OnPropertyChanged(); }
+        }
+
+        private Category _selectedCategory;
+        public Category SelectedCategory
+        {
+            get => _selectedCategory;
+            set { _selectedCategory = value; OnPropertyChanged(); }
+        }
+
+        private decimal _amount;
+        public decimal Amount
+        {
+            get => _amount;
+            set { _amount = value; OnPropertyChanged(); }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set { _description = value; OnPropertyChanged(); }
+        }
+
+        private DateTime _date = DateTime.Today;
+        public DateTime Date
+        {
+            get => _date;
+            set { _date = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<DataModel> _transactions;
+        public ObservableCollection<DataModel> Transactions
+        {
+            get => _transactions;
+            set { _transactions = value; OnPropertyChanged(); }
+        }
+
+        private void AddTransaction()
+        {
+            var user = UserSession.CurrentUser;
+            if (user == null) return;
+
+            if (user.History == null)
+                user.History = new List<DataModel>();
+
+            var data = new DataModel
             {
-                Transactions = new ObservableCollection<DataModel>();
-            }
+                TransactionType = SelectedTransactionType,
+                Amount = Amount,
+                Description = Description,
+                Category = SelectedCategory,
+                Date = Date
+            };
+
+            user.History.Add(data);
+            _allTransactions.Add(data);
+
+            UserService.SaveUser(user);
+
+            ApplyFilter();
             UpdateData();
 
+            SelectedTransactionType = TransactionType.Expense;
+            SelectedCategory = Category.Food;
+            Amount = 0;
+            Description = string.Empty;
+            Date = DateTime.Today;
+        }
+
+        private void DeleteTransaction(object obj)
+        {
+            if (obj is DataModel data)
+            {
+                var result = MessageBox.Show(
+                    "Are you sure you want to delete this transaction?",
+                    "Confirm Delete",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Warning);
+
+                if (result != MessageBoxResult.Yes)
+                    return;
+
+                var user = UserSession.CurrentUser;
+                if (user == null) return;
+
+                user.History.Remove(data);
+                _allTransactions.Remove(data);
+
+                UserService.SaveUser(user);
+
+                ApplyFilter();
+                UpdateData();
+            }
         }
 
         private void ClearFilter()
@@ -50,258 +151,37 @@ namespace Expense_Tracker.ViewModel
             TransactionTypeFilterCategory = null;
 
             Transactions = new ObservableCollection<DataModel>(_allTransactions);
-        }
 
-
-        public Array TransactionTypes => Enum.GetValues(typeof(TransactionType));
-        public Array Categories => Enum.GetValues(typeof(Category));
-
-        private TransactionType _selectedTransactionType;
-
-        public TransactionType SelectedTransactionType
-        {
-            get => _selectedTransactionType;
-            set
-            {
-                _selectedTransactionType = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private Category _selectedCategory;
-
-        public Category SelectedCategory
-        {
-            get => _selectedCategory;
-            set
-            {
-                _selectedCategory = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private decimal _amount;
-
-        public decimal Amount
-        {
-            get => _amount;
-            set
-            {
-                _amount = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private string _description;
-
-        public string Description
-        {
-            get => _description;
-            set
-            {
-                _description = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private DateTime _date = DateTime.Today;
-
-        public DateTime Date
-        {
-
-            get => _date;
-            set
-            {
-                _date = value;
-                OnPropertyChanged();
-            }
-        }
-
-
-        private void AddTransaction()
-        {
-            var user = UserSession.CurrentUser;
-
-            if (user == null)
-                return;
-
-            if (user.History == null)
-                user.History = new List<DataModel>();
-
-            var data = new DataModel
-            {
-                TransactionType = SelectedTransactionType,
-                Amount = (decimal)Amount,
-                Description = Description,
-                Category = SelectedCategory,
-                Date = Date
-            };
-
-            user.History.Add(data);
-
-            Transactions.Add(data);
-
-            UserService.SaveUser(user);
             UpdateData();
-
-            SelectedTransactionType = TransactionType.Expense;
-            SelectedCategory = Category.Food;
-            Amount = 0;
-            Description = string.Empty;
-            Date = DateTime.Today;
-        }
-
-        private ObservableCollection<DataModel> _transactions;
-
-        public ObservableCollection<DataModel> Transactions
-        {
-            get => _transactions;
-            set
-            {
-                _transactions = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private void DeleteTransaction(object obj)
-        {
-            if (obj is DataModel data)
-            {
-
-                var result = MessageBox.Show(
-                           "Are you sure you want to delete this transaction?",
-                           "Confirm Delete",
-                           MessageBoxButton.YesNo,
-                           MessageBoxImage.Warning);
-
-                if (result != MessageBoxResult.Yes)
-                    return;
-
-                var user = UserSession.CurrentUser;
-
-                if (user == null)
-                    return;
-
-                user.History.Remove(data);
-                Transactions.Remove(data);
-
-                UserService.SaveUser(user);
-                UpdateData();
-                ApplyFilter();
-            }
-        }
-
-        private decimal _balance;
-
-
-        public decimal Balance
-        {
-            get => _balance;
-            set
-            {
-                _balance = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public decimal _expense;
-
-        public decimal Expense
-        {
-            get => _expense;
-            set
-            {
-                _expense = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public decimal _income;
-
-        public decimal Income
-        {
-            get => _income;
-            set
-            {
-                _income = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private int _numberOfIncome;
-
-        public int NumberOfIncome
-        {
-            get => _numberOfIncome;
-            set
-            {
-                _numberOfIncome = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private int _numberOfExpense;
-
-        public int NumberOfExpense
-        {
-            get => _numberOfExpense;
-            set
-            {
-                _numberOfExpense = value;
-                OnPropertyChanged();
-
-            }
         }
 
         private DateTime? _fromDate;
         public DateTime? FromDate
         {
             get => _fromDate;
-            set
-            {
-                _fromDate = value;
-                OnPropertyChanged();
-                ApplyFilter();
-            }
+            set { _fromDate = value; OnPropertyChanged(); ApplyFilter(); }
         }
-
 
         private DateTime? _toDate;
         public DateTime? ToDate
         {
             get => _toDate;
-            set
-            {
-                _toDate = value;
-                OnPropertyChanged();
-                ApplyFilter();
-            }
+            set { _toDate = value; OnPropertyChanged(); ApplyFilter(); }
         }
 
         private Category? _selectedFilterCategory;
         public Category? SelectedFilterCategory
         {
             get => _selectedFilterCategory;
-            set
-            {
-                _selectedFilterCategory = value;
-                OnPropertyChanged();
-                ApplyFilter();
-            }
+            set { _selectedFilterCategory = value; OnPropertyChanged(); ApplyFilter(); }
         }
 
         private TransactionType? _transactionTypeFilterCategory;
         public TransactionType? TransactionTypeFilterCategory
         {
             get => _transactionTypeFilterCategory;
-            set
-            {
-                _transactionTypeFilterCategory = value;
-                OnPropertyChanged();
-                ApplyFilter();
-            }
+            set { _transactionTypeFilterCategory = value; OnPropertyChanged(); ApplyFilter(); }
         }
-
 
         private void ApplyFilter()
         {
@@ -320,8 +200,44 @@ namespace Expense_Tracker.ViewModel
                 query = query.Where(t => t.Category == SelectedFilterCategory.Value);
 
             Transactions = new ObservableCollection<DataModel>(query);
+
+            UpdateData();
         }
 
+        private decimal _balance;
+        public decimal Balance
+        {
+            get => _balance;
+            set { _balance = value; OnPropertyChanged(); }
+        }
+
+        private decimal _expense;
+        public decimal Expense
+        {
+            get => _expense;
+            set { _expense = value; OnPropertyChanged(); }
+        }
+
+        private decimal _income;
+        public decimal Income
+        {
+            get => _income;
+            set { _income = value; OnPropertyChanged(); }
+        }
+
+        private int _numberOfIncome;
+        public int NumberOfIncome
+        {
+            get => _numberOfIncome;
+            set { _numberOfIncome = value; OnPropertyChanged(); }
+        }
+
+        private int _numberOfExpense;
+        public int NumberOfExpense
+        {
+            get => _numberOfExpense;
+            set { _numberOfExpense = value; OnPropertyChanged(); }
+        }
 
         private void UpdateData()
         {
@@ -350,11 +266,9 @@ namespace Expense_Tracker.ViewModel
                 .Count(t => t.TransactionType == TransactionType.Expense);
 
             Balance = Income - Expense;
-            if (Balance <= 0)
-            {
-                Balance = 0;
-            }
-        }
 
+            if (Balance < 0)
+                Balance = 0;
+        }
     }
 }
